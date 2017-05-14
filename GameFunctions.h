2f90ac6321b59019ceb76c1fe913424bd4d6bdc2
Calcnerd <calcnerd.ti.cep.d@gmail.com>
2017-05-14 16:09:43 -0600

    Create GameFunctions.h

---
+++ GameFunctions.h
@@ -0,0 +1,11 @@
+#ifndef _KeyFunctions_h
+#define _KeyFunctions_h
+
+void KeyLeft(int x);
+void KeyRight(int x);
+bool ShootGun(int x);
+void ZombieDead(int x);
+struct NewZombie(struct Z);
+
+
+#endif
