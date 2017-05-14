77387fb035edbf52c2977d80df44c51d5c2612f2
Calcnerd <calcnerd.ti.cep.d@gmail.com>
2017-05-14 16:42:32 -0600

    Create xlibcsprite.h

---
+++ xlibcsprite.h
@@ -0,0 +1,15 @@
+// Converted using ConvPNG
+// This file contains all the graphics sources for easier inclusion in a project
+#ifndef xlibcsprites_H
+#define xlibcsprites_H
+#include <stdint.h>
+
+extern uint8_t BloodSplatter_data[227];
+#define BloodSplatter ((gfx_image_t*)BloodSplatter_data)
+extern uint8_t SilencedPistol_data[3992];
+#define SilencedPistol ((gfx_image_t*)SilencedPistol_data)
+extern uint8_t SilencedPistolFiring_data[4904];
+#define SilencedPistolFiring ((gfx_image_t*)SilencedPistolFiring_data)
+extern uint16_t xlibcsprites_pal[37];
+
+#endif
