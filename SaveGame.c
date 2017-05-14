f1d630611fcec5ddaa56e3ee82e0ca91f0870758
Calcnerd <calcnerd.ti.cep.d@gmail.com>
2017-05-14 16:16:37 -0600

    Create SaveGame.c

---
+++ SaveGame.c
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <tice.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <fileioc.h>
+
+#include "SaveGame.h"
+
+//void Save_Game(void){
+//    ti_CloseAll();
+//    if (Mode=2 /*endless*/)
+//}
