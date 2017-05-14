a92da3cdbc631977b009b5f8288b3e3588b86eca
Calcnerd <calcnerd.ti.cep.d@gmail.com>
2017-05-14 16:30:19 -0600

    Create Tests.h

---
+++ Tests.h
@@ -0,0 +1,9 @@
+#ifndef _Tests_h
+#define _Tests_h
+
+/*simple functions that check things*/
+bool CheckInScreen(int x);
+bool CheckInGun(int x);
+void AddBloodSplatter(int x, int ShowBloodSplatter0, int BloodSplatter1);
+
+#endif
