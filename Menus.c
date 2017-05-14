6353a45dccf04952845f79adadba807add48284c
Calcnerd <calcnerd.ti.cep.d@gmail.com>
2017-05-14 16:13:08 -0600

    Create Menus.c

---
+++ Menus.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <tice.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "SaveGame.h"
+
+int Main_Menu(void);
+int Ingame_Menu(void);
+int Ingame_Exit(void);
