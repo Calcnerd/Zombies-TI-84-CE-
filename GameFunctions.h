6ff784428894fa5d04c68deb754118ccb09592b9
Calcnerd <calcnerd.ti.cep.d@gmail.com>
2017-05-18 17:44:24 -0600

    Update GameFunctions.h

--- GameFunctions.h
+++ GameFunctions.h
@@ -1,11 +1,12 @@
-#ifndef _KeyFunctions_h
-#define _KeyFunctions_h
+#ifndef _GameFunctions_h
+#define _GameFunctions_h
 
-void KeyLeft(int x);
-void KeyRight(int x);
+void KeyLeft(int* x, bool* WithinScreen, bool* WithinGun, int* ShowFiring, int* CanShoot, int* ShowBloodSplatter);
+void KeyRight(int* x, bool* WithinScreen, bool* WithinGun, int* ShowFiring, int* CanShoot, int* ShowBloodSplatter);
 bool ShootGun(int x);
 void ZombieDead(int x);
-struct NewZombie(struct Z);
+struct zombie NewZombie(void);
+void update(int x, int ammo);
 
 
 #endif
