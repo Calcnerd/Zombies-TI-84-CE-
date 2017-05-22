611c56ecaf1bdf8135f07d7a3d01235c21bdf3a0
Calcnerd <calcnerd.ti.cep.d@gmail.com>
2017-05-22 11:26:44 -0600

    Update Tests.c

--- Tests.c
+++ Tests.c
@@ -7,30 +7,28 @@
 #include <stdlib.h>
 #include <string.h>
 
-bool CheckInScreen(int x){
-    bool WithinScreen;
-    if (x+225>=0 && x<=360){
-        WithinScreen=true;
-    }else{
-        WithinScreen=false;
-    }
-    return WithinScreen;
-}
-
-bool CheckInGun(int x){
-    bool WithinGun;
-    if (x+225>=188 && x<=131){
-        WithinGun=true;
-    }else{
-        WithinGun=false;
-    }
-    return WithinGun;
-}
-
-void AddBloodSplatter(int x, int ShowBloodSplatter0, int ShowBloodSplatter1){
-        if (!ShowBloodSplatter0){
-            ShowBloodSplatter0=160-x+50;
+void AddBloodSplatter(int x, int* ShowBloodSplatter/*, int ShowBloodSplatter*/)
+{
+        if (!ShowBloodSplatter[0])
+        {
+            ShowBloodSplatter[0]=160-x-3;
         }else{
-            ShowBloodSplatter1=160-x+50;
+            ShowBloodSplatter[1]=160-x-3;
         }
+}
+
+void CheckRange(int x, bool* WithinScreen, bool* WithinGun)
+{
+    if (x+225>=0 && x<=320)
+    {
+        *WithinScreen=true;
+    }else{
+        *WithinScreen=false;
+    }
+    if (x+225>=188 && x<=131)
+    {
+        *WithinGun=true;
+    }else{
+        *WithinGun=false;
+    }
 }
