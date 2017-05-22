e83b08d337634cfee02cccf32c553654a3c7f8cb
Calcnerd <calcnerd.ti.cep.d@gmail.com>
2017-05-22 11:27:37 -0600

    Update main.c

--- main.c
+++ main.c
@@ -27,9 +27,6 @@
 #include "GameFunctions.h"
 
 
-bool shoot_gun(void);
-
-
 void main()
 {
     player Player;
@@ -43,22 +40,23 @@ void main()
     Zombie.ShowBloodSplatter[0]=0;
     Zombie.ShowBloodSplatter[1]=0;
     Zombie.zType=NORMAL;
-    Player.ammo=10;
+    Player.ammo=100;
     Player.health=100;
     gfx_Begin(gfx_8bpp);
     gfx_SetDrawBuffer();
     gfx_SetColor(255);
-    gfx_FillScreen(255);
+    gfx_FillScreen(0);
     gfx_SetPalette(sprites_pal,sizeof(sprites_pal),0);
     gfx_TransparentSprite(EnemySprite,Zombie.x,30);
     gfx_SetPalette(xlibcsprites_pal,sizeof(xlibcsprites_pal),0);
     gfx_TransparentSprite(SilencedPistol,131,174);
     update(Zombie.x, Player.ammo);
+    //main game loop
     while(kb_ScanGroup(kb_group_6) != kb_Clear)
     {
         if (kTurn & kb_Left)
         {
-            CheckRange(Zombie.x, Zombie.WithinScreen, Zombie.WithinGun);
+            CheckRange(Zombie.x, &Zombie.WithinScreen, &Zombie.WithinGun);
             if (ShowFiring)
                 --ShowFiring;
             if (CanShoot)
@@ -73,34 +71,13 @@ void main()
             }else{
                 Zombie.x+=8;
             }
-            CheckRange(Zombie.x, Zombie.WithinScreen, Zombie.WithinGun);
-            if (Zombie.WithinScreen)
-            {
-                gfx_SetPalette(sprites_pal,sizeof(sprites_pal),0);
-                gfx_TransparentSprite(EnemySprite,Zombie.x,30);
-                gfx_SetPalette(xlibcsprites_pal,sizeof(xlibcsprites_pal),0);
-                for(i=0;i<2;i++)
-                {
-                    if (Zombie.ShowBloodSplatter[i])
-                    {
-                        gfx_TransparentSprite(BloodSplatter,Zombie.ShowBloodSplatter[i]+Zombie.x,100);
-                    }
-                }
-                if (Zombie.WithinGun)
-                {
-                    if (ShowFiring)
-                    {
-                        gfx_TransparentSprite(SilencedPistolFiring,131,154);
-                    }else{
-                        gfx_TransparentSprite(SilencedPistol,131,174);
-                    }
-                }
-            }
+            CheckRange(Zombie.x, &Zombie.WithinScreen, &Zombie.WithinGun);
+            Sprite(Zombie.x, Zombie.ShowBloodSplatter, Zombie.WithinScreen, Zombie.WithinGun, ShowFiring, Zombie.health);
             update(Zombie.x, Player.ammo);
         }
         if (kTurn & kb_Right)
         {
-            CheckRange(Zombie.x, Zombie.WithinScreen, Zombie.WithinGun);
+            CheckRange(Zombie.x, &Zombie.WithinScreen, &Zombie.WithinGun);
             if (ShowFiring)
                 --ShowFiring;
             if (CanShoot)
@@ -115,34 +92,16 @@ void main()
             }else{
                 Zombie.x-=8;
             }
-            CheckRange(Zombie.x, Zombie.WithinScreen, Zombie.WithinGun);
-            if (Zombie.WithinScreen){
-                gfx_SetPalette(sprites_pal,sizeof(sprites_pal),0);
-                gfx_TransparentSprite(EnemySprite,Zombie.x,30);
-                gfx_SetPalette(xlibcsprites_pal,sizeof(xlibcsprites_pal),0);
-                for(i=0;i<2;i++){
-                    if (Zombie.ShowBloodSplatter[i])
-                    {
-                        gfx_TransparentSprite(BloodSplatter,Zombie.ShowBloodSplatter[i]+Zombie.x,100);
-                    }
-                }
-                if (Zombie.WithinGun){
-                    if (ShowFiring)
-                    {
-                        gfx_TransparentSprite(SilencedPistolFiring,131,154);
-                    }else{
-                        gfx_TransparentSprite(SilencedPistol,131,174);
-                    }
-                }
-            }
+            CheckRange(Zombie.x, &Zombie.WithinScreen, &Zombie.WithinGun);
+            Sprite(Zombie.x, Zombie.ShowBloodSplatter, Zombie.WithinScreen, Zombie.WithinGun, ShowFiring, Zombie.health);
             update(Zombie.x, Player.ammo);
         }
         if ((kShoot & kb_2nd) && (!ShowFiring && !CanShoot)){
-            ShowFiring=10;
             if (Player.ammo)
             {
+                ShowFiring=10;
                 --Player.ammo;
-                if (shoot_gun())
+                if (shoot_gun(Zombie.x, Zombie.ShowBloodSplatter, &Zombie.WithinScreen, &Zombie.WithinGun, ShowFiring, Zombie.health))
                 {
                     AddBloodSplatter(Zombie.x, Zombie.ShowBloodSplatter);
                     Zombie.health-=50;
@@ -153,7 +112,6 @@ void main()
             update(Zombie.x, Player.ammo);
         }
         if (ShowFiring){
-            gfx_SetPalette(xlibcsprites_pal,sizeof(xlibcsprites_pal),0);
             gfx_TransparentSprite(SilencedPistolFiring,131,154);
             gfx_BlitBuffer();
             --ShowFiring;
@@ -161,24 +119,20 @@ void main()
             {
                 if (Zombie.WithinGun)
                 {
-                    gfx_SetPalette(sprites_pal,sizeof(sprites_pal),0);
-                    gfx_TransparentSprite(EnemySprite,Zombie.x,30);
+                    Sprite(Zombie.x, Zombie.ShowBloodSplatter, Zombie.WithinScreen, Zombie.WithinGun, ShowFiring, Zombie.health);
                 }else{
                     for(i=0;i<50;i++)
                         gfx_FillRectangle(131,154,57,86);
                 }
-                gfx_SetPalette(xlibcsprites_pal,sizeof(xlibcsprites_pal),0);
                 gfx_TransparentSprite(SilencedPistol,131,172);
                 gfx_BlitBuffer();
                 if (Zombie.WithinGun)
                 {
-                    gfx_SetPalette(sprites_pal,sizeof(sprites_pal),0);
-                    gfx_TransparentSprite(EnemySprite,Zombie.x,30);
+                    Sprite(Zombie.x, Zombie.ShowBloodSplatter, Zombie.WithinScreen, Zombie.WithinGun, ShowFiring, Zombie.health);
                 }else{
                     for(i=0;i<50;i++)
                         gfx_FillRectangle(131,172,57,70);
                 }
-                gfx_SetPalette(xlibcsprites_pal,sizeof(xlibcsprites_pal),0);
                 gfx_TransparentSprite(SilencedPistol,131,174);
                 gfx_BlitBuffer();
                 CanShoot=10;
@@ -193,43 +147,4 @@ void main()
     }
     gfx_End();
     pgrm_CleanUp();
-}
-
-/* Other functions */
-
-bool shoot_gun(void){
-    bool hit;
-    int i;
-    Zombie.WithinScreen=CheckInScreen(Zombie.x);
-    Zombie.WithinGun=CheckInGun(Zombie.x);
-    //gun moving upward
-    if (Zombie.WithinGun)
-    {
-        gfx_SetPalette(sprites_pal,sizeof(sprites_pal),0);
-        gfx_TransparentSprite(EnemySprite,Zombie.x,30);
-    }else{
-        for(i=0;i<50;i++)
-            gfx_FillRectangle(131,174,57,70);
-    }
-    gfx_SetPalette(xlibcsprites_pal,sizeof(xlibcsprites_pal),0);
-    gfx_TransparentSprite(SilencedPistol,131,172);
-    gfx_BlitBuffer();
-    if (Zombie.WithinGun)
-    {
-        gfx_SetPalette(sprites_pal,sizeof(sprites_pal),0);
-        gfx_TransparentSprite(EnemySprite,Zombie.x,30);
-    }else{
-        for(i=0;i<50;i++)
-            gfx_FillRectangle(131,172,57,70);
-    }
-    gfx_SetPalette(xlibcsprites_pal,sizeof(xlibcsprites_pal),0);
-    gfx_TransparentSprite(SilencedPistolFiring,131,154);
-    gfx_BlitBuffer();
-    if(Zombie.x+50<=158 && Zombie.x+175>=162)
-    {
-        hit=true;
-    }else{
-        hit=false;
-    }
-    return hit;
 }
