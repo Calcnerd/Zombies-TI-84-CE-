4d7ba76ea559aef014391c47a9053394104ed491
Calcnerd <calcnerd.ti.cep.d@gmail.com>
2017-06-02 16:36:49 -0600

    Update GameFunctions.h

--- GameFunctions.h
+++ GameFunctions.h
@@ -1,10 +1,10 @@
 #ifndef _GameFunctions_h
 #define _GameFunctions_h
 
-bool shoot_gun(int x, int ShowBloodSplatter[], bool* WithinScreen, bool* WithinGun, int ShowFiring, int health);
+bool shoot_gun(int x, int ShowBloodSplatter[], bool* WithinScreen, bool* WithinGun, bool* GunRedraw, int ShowFiring, int health);
 void ZombieDead(int x);
 //experimental function for creating a new zombie
-void NewZombie(int* x, int* health, int* ShowBloodSplatter[], enum TYPE *zType);
+void NewZombie(int* x, int* health, int* ShowBloodSplatter, enum TYPE *zType);
 void update(int x, int ammo);
 void Sprite(int x, int ShowBloodSplatter[], int WithinScreen, int WithinGun, int ShowFiring, int Health);
 
